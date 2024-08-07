051609720d9491419914fc7a146ce878841eccb0
Nguyen Minh Khai <95909601+khaiv52@users.noreply.github.com>
2024-08-07 13:39:30 +0700

    Update AppConfig.java

--- src/main/java/com/vanlang/lyxircaspb/config/AppConfig.java
+++ src/main/java/com/vanlang/lyxircaspb/config/AppConfig.java
@@ -49,7 +49,8 @@ public class AppConfig {
         cfg.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:4200",
-                "https://lyxirca-shop.vercel.app"
+                "https://lyxirca-shop.vercel.app",
+                "https://lyxrica-be.onrender.com"
         ));
         cfg.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
